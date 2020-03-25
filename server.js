ec7b99ce7720a3247990924e9b497eb4b32e4d22
ensemblelab-lenovo <buchilazarus4@gmail.com>
2020-03-25 13:52:52 +0100

    Inital set up

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const dotenv = require('dotenv')
+
+const app = express()
+
+dotenv.config({path: './config/config.env'})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
+
