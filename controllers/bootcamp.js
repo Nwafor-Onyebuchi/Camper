e5032c64948f1aa4608aa9201926a733470fac7b
buchi <buchilazarus4@gmail.com>
2020-08-22 06:30:28 +0100

    Relationships between models

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -41,9 +41,22 @@ exports.getOneBootcamp = async (req, res, next) => {
   }
 };
 
-exports.createBootcamp = async (req, res) => {
+exports.createBootcamp = async (req, res, next) => {
   try {
+    //Add a user to bootcamp
     req.body.user = req.user.id;
+
+    // check for created bootcamps
+    const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+    if (publishedBootcamp && req.user.role !== "admin") {
+      return next(
+        new ErrorResponse(
+          `The user with ID ${req.user.id} has already published a bootcamp`,
+          401
+        )
+      );
+    }
+
     const bootcamp = await Bootcamp.create(req.body);
     res.status(201).json({
       success: true,
--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: "./config/config.env" });
 
 const Bootcamp = require("./models/Bootcamps");
 const Course = require("./models/Course");
+const User = require("./models/User");
 
 // Connect to DB
 mongoose.connect(`mongodb://localhost/camper`, {
@@ -20,40 +21,47 @@ mongoose.connect(`mongodb://localhost/camper`, {
 
 // Read JSON file
 const bootcamps = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
 );
 const courses = JSON.parse(
-    fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
-  );
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+);
+
+const user = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
+);
 
 // Import data
 
-const importData = async() => {
-    try {
-        await Bootcamp.create(bootcamps)
-      /await Course.create(courses)
-        console.log('Data imported successfully')
-        process.exit()
-    } catch (error) {
-        console.error(error)
-    }
-}
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    await Course.create(courses);
+    await User.create(user);
+
+    console.log("Data imported successfully");
+    process.exit();
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 //Delete data
-const deleteData = async() => {
-    try {
-        await Bootcamp.deleteMany()
-       await Course.deleteMany()
-        console.log('Data deleted successfully')
-        process.exit()
-    } catch (error) {
-        console.error(error)
-    }
-}
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    await Course.deleteMany();
+    await User.deleteMany();
 
-if(process.argv[2] === '-i') {
-    importData()
-} else if(process.argv[2] === '-d') {
-    deleteData()
-    
+    console.log("Data deleted successfully");
+    process.exit();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+if (process.argv[2] === "-i") {
+  importData();
+} else if (process.argv[2] === "-d") {
+  deleteData();
 }
